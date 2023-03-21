853b4b062cc936e2d2880e243020d4b9b0b5f0ea
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-21 12:51:25 +0530

    Added: Add Duplicate Value

---
+++ Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem_LINQ
+{
+    public class Contact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public double phoneNumber { get; set; }
+        public int zipCode { get; set; }
+        public string emailId { get; set; }
+
+        public int contactId { get; set; }
+        public string contactType { get; set; }
+    }
+}
